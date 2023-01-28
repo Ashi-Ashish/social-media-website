a78e415d31375b411f958829c2161925961c05ec
AshishRalh <46755420+AshishRalh@users.noreply.github.com>
2023-01-27 23:45:15 -0500

    Account Settings

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -8,6 +8,10 @@ from .models import Profile
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'setting.html ')
+
 def signup(request):
 
     if request.method == 'POST':
@@ -28,12 +32,14 @@ def signup(request):
                 user.save()
 
                 #Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
